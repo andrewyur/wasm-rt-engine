ef3431375118669bc70d52f55adbca1c70aa6610
Andrew Yurovchak <andy@yurovchak.net>
2024-12-21 14:43:22 -0500

    add flake

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # wasm-rt-engine
+
+A real time raytracing engine built with WASM, targeting the browser
+
+## TODO
+
+- [ ] set up rust/wasm profile in vscode & nix shell
+- [ ] set up a crate and wasm-bindgen
+- [ ] read the wgpu book
+
+## Resources
+
+- <https://sotrh.github.io/learn-wgpu/#special-thanks-to-these-patrons>
+- <https://raytracing.github.io/>
+- <https://blog.demofox.org/2020/05/25/casual-shadertoy-path-tracing-1-basic-camera-diffuse-emissive/>
---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {self, flake-utils, nixpkgs}: 
+        flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+        in {
+            devShell = pkgs.mkShell {
+                packages = with pkgs; [
+                    cargo
+                    clippy
+                    rustc
+                    rustfmt
+                ];
+                shellHook = "zsh";
+            };
+        }
+    );
+}
